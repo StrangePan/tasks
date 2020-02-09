78aeea580ee497a162d83fa26ffc0a3103d03911
Dan Andrus <danieleandrus@gmail.com>
2020-02-08 22:50:17 -0800

    Update RemoveHandler to use TaskStore

--- src/tasks/cli/handlers/RemoveHandler.java
+++ src/tasks/cli/handlers/RemoveHandler.java
@@ -1,64 +1,40 @@
 package tasks.cli.handlers;
 
 import static java.util.stream.Collectors.joining;
-import static omnia.algorithm.SetAlgorithms.differenceBetween;
-import static omnia.data.stream.Collectors.toSet;
 
-import omnia.data.structure.DirectedGraph;
+import io.reactivex.Observable;
 import omnia.data.structure.Set;
-import omnia.data.structure.immutable.ImmutableDirectedGraph;
 import omnia.data.structure.immutable.ImmutableSet;
-import tasks.Task;
-import tasks.cli.CliTaskId;
 import tasks.cli.arg.RemoveArguments;
+import tasks.model.Task;
+import tasks.model.TaskStore;
 
 public final class RemoveHandler implements ArgumentHandler<RemoveArguments> {
   @Override
   public void handle(RemoveArguments arguments) {
-    Set<CliTaskId> specifiedIds = ImmutableSet.<CliTaskId>builder().addAll(arguments.tasks()).build();
-
     // Validate arguments
-    if (!specifiedIds.isPopulated()) {
+    if (!arguments.tasks().isPopulated()) {
       throw new HandlerException("no tasks specified");
     }
 
-    DirectedGraph<Task> taskGraph = HandlerUtil.loadTasks();
-    Set<DirectedGraph.Node<Task>> targetTaskNodes =
-        taskGraph.nodes()
-            .stream()
-            .filter(n -> specifiedIds.contains(n.item().id()))
-            .collect(toSet());
-    Set<Task> targetTasks =
-        targetTaskNodes.stream()
-            .map(DirectedGraph.Node::item)
-            .collect(toSet());
+    TaskStore taskStore = HandlerUtil.loadTaskStore();
 
-    if (targetTasks.count() != specifiedIds.count()) {
-      // likely specified a task ID that doesn't exist. report which ones are wrong.
-      Set<CliTaskId> unknownIds =
-          differenceBetween(
-              specifiedIds,
-              targetTasks.stream().map(Task::id).collect(toSet()));
-      String listOfUnknownIds =
-          unknownIds.stream()
-              .map(Object::toString)
-              .collect(joining(", "));
-      throw new HandlerException("unknown task id(s) specified: " + listOfUnknownIds);
-    }
+    HandlerUtil.validateTasksIds(taskStore, arguments.tasks());
 
-    ImmutableDirectedGraph.Builder<Task> newTaskBuilder =
-        ImmutableDirectedGraph.buildUpon(taskGraph);
+    Set<Task> tasksToDelete =
+        ImmutableSet.copyOf(HandlerUtil.toTasks(taskStore, arguments.tasks()).blockingIterable());
 
-    // remove the target tasks from the graph
-    targetTasks.forEach(newTaskBuilder::removeNode);
+    String report = "tasks marked as completed:" + stringify(tasksToDelete);
 
-    HandlerUtil.writeTasks(newTaskBuilder.build());
+    Observable.fromIterable(tasksToDelete)
+        .flatMapCompletable(taskStore::deleteTask)
+        .andThen(taskStore.writeToDisk())
+        .blockingAwait();
 
-    System.out.println(
-        "task(s) marked as completed: "
-            + targetTasks.stream()
-            .map(Task::id)
-            .map(Object::toString)
-            .collect(joining(", ")));
+    System.out.println(report);
+  }
+
+  private static String stringify(Set<Task> tasks) {
+    return tasks.stream().map(Task::toString).map(line -> "\n  " + line).collect(joining());
   }
 }
