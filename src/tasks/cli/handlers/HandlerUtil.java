74e3c53c518b1af6952432a52d7d8237713309d4
Dan Andrus <danieleandrus@gmail.com>
2020-02-08 23:22:41 -0800

    Delete all the deprecated code

--- src/tasks/cli/handlers/HandlerUtil.java
+++ src/tasks/cli/handlers/HandlerUtil.java
@@ -5,13 +5,11 @@ import static java.util.stream.Collectors.joining;
 import io.reactivex.Observable;
 import java.util.EnumMap;
 import omnia.data.structure.Collection;
-import omnia.data.structure.DirectedGraph;
 import omnia.data.structure.Set;
 import omnia.data.structure.immutable.ImmutableSet;
-import tasks.Task;
 import tasks.cli.CliTaskId;
-import tasks.io.File;
-import tasks.io.TaskStore;
+import tasks.model.Task;
+import tasks.model.TaskStore;
 import tasks.model.impl.TaskStoreImpl;
 
 final class HandlerUtil {
@@ -21,19 +19,11 @@ final class HandlerUtil {
   private static final String FILE_NAME = "asdf";
   private static final Object TASK_NOT_FOUND = new Object();
 
-  private static File file() {
-    return File.fromPath(FILE_NAME);
-  }
-
-  static DirectedGraph<Task> loadTasks() {
-    return new TaskStore(file()).retrieveBlocking();
-  }
-
-  static tasks.model.TaskStore loadTaskStore() {
+  static TaskStore loadTaskStore() {
     return new TaskStoreImpl(FILE_NAME);
   }
 
-  static void validateTasksIds(tasks.model.TaskStore store, Iterable<CliTaskId> ids) {
+  static void validateTasksIds(TaskStore store, Iterable<CliTaskId> ids) {
     Set<CliTaskId> invalidTaskIds =
         ImmutableSet.copyOf(
             Observable.fromIterable(ids)
@@ -51,27 +41,23 @@ final class HandlerUtil {
     }
   }
 
-  static Observable<tasks.model.Task> toTasks(tasks.model.TaskStore store, Iterable<CliTaskId> ids) {
+  static Observable<Task> toTasks(TaskStore store, Iterable<CliTaskId> ids) {
     return Observable.fromIterable(ids)
         .flatMapMaybe(id -> store.lookUpById(id.asLong()));
-  }
-
-  static void writeTasks(DirectedGraph<Task> tasks) {
-    new TaskStore(file()).storeBlocking(tasks);
   }
 
   static String stringifyContents(Collection<?> collection) {
     return collection.stream().map(Object::toString).collect(joining(","));
   }
 
-  static EnumMap<CompletedState, Set<tasks.model.Task>> groupByCompletionState(
-      Observable<tasks.model.Task> tasks) {
+  static EnumMap<CompletedState, Set<Task>> groupByCompletionState(
+      Observable<Task> tasks) {
     return tasks
         .groupBy(task -> task.isCompleted().blockingFirst()
             ? CompletedState.COMPLETE
             : CompletedState.INCOMPLETE)
         .reduce(
-            new EnumMap<CompletedState, Set<tasks.model.Task>>(CompletedState.class),
+            new EnumMap<CompletedState, Set<Task>>(CompletedState.class),
             (map, taskSet) -> {
               map.put(taskSet.getKey(), ImmutableSet.copyOf(taskSet.blockingIterable()));
               return map;
