cfeb6d393d8e27d192fefb3b420f3c7a1c23bf19
Dan Andrus <danieleandrus@gmail.com>
2020-02-08 21:01:50 -0800

    Rewrite InfoHandler to use new TaskStore

--- src/tasks/cli/handlers/InfoHandler.java
+++ src/tasks/cli/handlers/InfoHandler.java
@@ -1,63 +1,43 @@
 package tasks.cli.handlers;
 
 import static java.util.stream.Collectors.joining;
-import static omnia.algorithm.SetAlgorithms.differenceBetween;
-import static omnia.data.stream.Collectors.toSet;
 
-import omnia.data.structure.DirectedGraph;
+import io.reactivex.Flowable;
 import omnia.data.structure.Set;
-import omnia.data.structure.immutable.ImmutableSet;
-import tasks.Task;
-import tasks.cli.CliTaskId;
 import tasks.cli.arg.InfoArguments;
+import tasks.model.Task;
+import tasks.model.TaskStore;
 
 public final class InfoHandler implements ArgumentHandler<InfoArguments> {
   @Override
   public void handle(InfoArguments arguments) {
-    Set<CliTaskId> specifiedIds = ImmutableSet.<CliTaskId>builder().addAll(arguments.tasks()).build();
-
-    if (!specifiedIds.isPopulated()) {
+    if (!arguments.tasks().isPopulated()) {
       throw new HandlerException("no tasks specified");
     }
 
-    DirectedGraph<Task> taskGraph = HandlerUtil.loadTasks();
-    Set<DirectedGraph.Node<Task>> targetTaskNodes =
-        taskGraph.nodes()
-            .stream()
-            .filter(n -> specifiedIds.contains(n.item().id()))
-            .collect(toSet());
-    Set<Task> targetTasks =
-        targetTaskNodes.stream()
-            .map(DirectedGraph.Node::item)
-            .collect(toSet());
+    TaskStore taskStore = HandlerUtil.loadTaskStore();
+    HandlerUtil.validateTasksIds(taskStore, arguments.tasks());
 
-    if (targetTasks.count() != specifiedIds.count()) {
-      // likely specified a task ID that doesn't exist. report which ones are wrong.
-      Set<CliTaskId> unknownIds =
-          differenceBetween(
-              specifiedIds,
-              targetTasks.stream().map(Task::id).collect(toSet()));
-      String listOfUnknownIds =
-          unknownIds.stream()
-              .map(Object::toString)
-              .collect(joining(", "));
-      throw new HandlerException("unknown task id(s) specified: " + listOfUnknownIds);
-    }
-
-    System.out.println(
-        targetTaskNodes.stream()
+    String message =
+        HandlerUtil.toTasks(taskStore, arguments.tasks())
             .map(InfoHandler::stringify)
-            .collect(joining("\n\n")));
+            .reduce((first, second) -> first + "\n\n" + second)
+            .blockingGet();
+
+    System.out.println(message);
   }
 
-  private static String stringify(DirectedGraph.Node<Task> node) {
-    String dependencies =
-        node.successors().stream().map(n -> "\n  " + n.item()).collect(joining());
-    String dependents =
-        node.predecessors().stream().map(n -> "\n  " + n.item()).collect(joining());
+  private static String stringify(Task task) {
+    return task.toString()
+        + stringify(task.query().tasksBlockingThis())
+        + stringify(task.query().tasksBlockedByThis());
+  }
 
-    return node.item()
-        + (!dependencies.isEmpty() ? "Blocked by" + dependencies : "")
-        + (!dependents.isEmpty() ? "Blocking" + dependencies : "");
+  private static String stringify(Flowable<Set<Task>> tasks) {
+    return tasks.firstOrError().map(InfoHandler::stringify).blockingGet();
+  }
+
+  private static String stringify(Set<Task> tasks) {
+    return tasks.stream().map(Task::toString).map(line -> "\n  " + line).collect(joining());
   }
 }
