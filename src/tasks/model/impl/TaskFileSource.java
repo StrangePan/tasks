4e5ea5051c86d504872a14762c216a156c6ba61b
Dan Andrus <danieleandrus@gmail.com>
2020-02-09 22:38:25 -0800

    Add file parsing

--- src/tasks/model/impl/TaskFileSource.java
+++ src/tasks/model/impl/TaskFileSource.java
@@ -5,9 +5,13 @@ import static java.util.stream.Collectors.joining;
 import io.reactivex.Completable;
 import io.reactivex.Observable;
 import io.reactivex.Single;
+import java.io.BufferedReader;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.util.Comparator;
+import java.util.Optional;
+import java.util.stream.Stream;
 import omnia.data.structure.DirectedGraph;
 import omnia.data.structure.Map;
 import omnia.data.structure.Pair;
@@ -20,6 +24,7 @@ final class TaskFileSource {
   private static final int VERSION = 1;
   private static final String END_OF_LINE = "\n";
   private static final String TASK_FIELD_DELIMITER = ";";
+  private static final String TASK_ID_DELIMITER = ",";
   private static final int ID_RADIX = Character.MAX_RADIX;
 
   private final File file;
@@ -39,18 +44,102 @@ final class TaskFileSource {
   }
 
   private static Pair<DirectedGraph<TaskId>, Map<TaskId, TaskData>> parseTaskData(InputStream stream) {
-    // TODO
-    return Pair.of(ImmutableDirectedGraph.empty(), ImmutableMap.empty());
+    return Single.just(stream)
+        .map(InputStreamReader::new)
+        .map(BufferedReader::new)
+        .map(BufferedReader::lines)
+        .map(Stream::iterator)
+        .map(iterator -> (Iterable<String>) () -> iterator)
+        .flatMapObservable(Observable::fromIterable)
+        .filter(line -> !line.isBlank())
+        .collect(LineCollector::new, LineCollector::collect)
+        .map(LineCollector::build)
+        .blockingGet();
+  }
+
+  private static final class LineCollector {
+    private final ImmutableDirectedGraph.Builder<TaskId> graph = ImmutableDirectedGraph.builder();
+    private final ImmutableMap.Builder<TaskId, TaskData> tasks = ImmutableMap.builder();
+    private State state = State.PARSING_NOTHING;
+
+    private enum State {
+      PARSING_NOTHING,
+      PARSING_GRAPH,
+      PARSING_TASKS,
+    }
+
+    void collect(String line) {
+      maybeParseStateChange(line).ifPresentOrElse(
+          newState -> this.state = newState,
+          () -> parseToCollection(line));
+    }
+
+    private Optional<State> maybeParseStateChange(String line) {
+      if (line.matches("^# version \\d+$")) {
+        return Optional.of(State.PARSING_NOTHING);
+      } else if (line.equals("# tasks")) {
+        return Optional.of(State.PARSING_TASKS);
+      } else if (line.equals("# dependencies")) {
+        return Optional.of(State.PARSING_GRAPH);
+      }
+      return Optional.empty();
+    }
+
+    private void parseToCollection(String line) {
+      switch (state) {
+        case PARSING_GRAPH:
+          parseToGraph(line);
+          break;
+        case PARSING_TASKS:
+          parseToTasks(line);
+          break;
+        default:
+          throw new IllegalStateException("unexpected line in file: " + line);
+      }
+    }
+
+    private void parseToGraph(String line) {
+      String[] fields = line.split(TASK_FIELD_DELIMITER);
+      TaskId id = parseId(fields[0]);
+      Stream.of(fields[1].split(TASK_ID_DELIMITER))
+          .map(TaskFileSource::parseId)
+          .forEach(dependency -> graph.addEdge(dependency, id));
+      // TODO: ensure unique ids
+      // TODO: ensure we have data for all ids
+    }
+
+    private void parseToTasks(String line) {
+      String[] fields = line.split(TASK_FIELD_DELIMITER);
+      TaskId id = parseId(fields[0]);
+      boolean completed = Boolean.parseBoolean(fields[1]);
+      String label = unescapeLabel(fields[2]);
+      tasks.put(id, new TaskData(completed, label));
+      // TODO: ensure unique ids
+    }
+
+    Pair<DirectedGraph<TaskId>, Map<TaskId, TaskData>> build() {
+      return Pair.of(graph.build(), tasks.build());
+    }
+  }
+
+  private static TaskId parseId(String string) {
+    try {
+      return new TaskId(Long.parseLong(string, ID_RADIX));
+    } catch (NumberFormatException ex) {
+      // TODO make a parsing exception
+      throw new RuntimeException("invalid id: " + string, ex);
+    }
   }
 
   private void serializeTaskData(Pair<DirectedGraph<TaskId>, Map<TaskId, TaskData>> data, OutputStream stream) {
     Observable.just(
-            Observable.just("# version ", Integer.toString(VERSION), END_OF_LINE),
-            Observable.just("# tasks", END_OF_LINE),
+            Observable.just("# version " + VERSION),
+            Observable.just("# tasks"),
             serialize(data.second()),
-            Observable.just("# dependencies", END_OF_LINE),
+            Observable.just("# dependencies"),
             serialize(data.first()))
         .concatMap(o -> o)
+        .concatMap(line -> Observable.just(line, END_OF_LINE))
         .map(String::getBytes)
         .blockingForEach(stream::write);
   }
@@ -61,8 +150,7 @@ final class TaskFileSource {
         .flatMapObservable(Observable::fromIterable)
         .sorted(Comparator.comparing(entry -> entry.key().asLong()))
         .map(entry -> Pair.of(entry.key(), entry.value()))
-        .map(TaskFileSource::serialize)
-        .concatMap(line -> Observable.just(line, END_OF_LINE));
+        .map(TaskFileSource::serialize);
   }
 
   private static String serialize(Pair<TaskId, TaskData> task) {
@@ -71,7 +159,8 @@ final class TaskFileSource {
         .append(TASK_FIELD_DELIMITER)
         .append(task.second().isCompleted())
         .append(TASK_FIELD_DELIMITER)
-        .append(escapeField(task.second().label()))
+        .append(escapeLabel(task.second().label()))
+        .append(TASK_FIELD_DELIMITER)
         .toString();
   }
 
@@ -79,19 +168,20 @@ final class TaskFileSource {
     return Long.toString(id.asLong(), ID_RADIX);
   }
 
-  private static String escapeField(String field) {
-    StringBuilder builder = new StringBuilder();
+  private static String escapeLabel(String label) {
+    StringBuilder escapedLabel = new StringBuilder();
     int i = 0;
-    for (int j = 0; j <= field.length(); j++) {
-      boolean isEscapableCharacter = j < field.length() && isEscapableCharacter(field.charAt(j));
-      if (j == field.length() || isEscapableCharacter) {
-        builder.append(field, i, j);
+    for (int j = 0; j <= label.length(); j++) {
+      boolean isEscapableCharacter = j < label.length() && isEscapableCharacter(label.charAt(j));
+      if (j == label.length() || isEscapableCharacter) {
+        escapedLabel.append(label, i, j);
+        i = j + 1;
       }
-      if (j < field.length() && isEscapableCharacter) {
-        builder.append("\\").append(escapeCharacter(field.charAt(j)));
+      if (j < label.length() && isEscapableCharacter) {
+        escapedLabel.append("\\").append(escapeCharacter(label.charAt(j)));
       }
     }
-    return builder.toString();
+    return escapedLabel.toString();
   }
 
   private static boolean isEscapableCharacter(char c) {
@@ -100,8 +190,9 @@ final class TaskFileSource {
       case '\n':
       case ';':
         return true;
+      default:
+        return false;
     }
-    return false;
   }
 
   private static char escapeCharacter(char c) {
@@ -112,8 +203,39 @@ final class TaskFileSource {
         return 'n';
       case ';':
         return ':';
+      default:
+        return c;
     }
-    return c;
+  }
+
+  private static String unescapeLabel(String escapedLabel) {
+    StringBuilder label = new StringBuilder();
+    int i = 0;
+    for (int j = 0; j <= escapedLabel.length(); j++) {
+      boolean isEscapeCharacter = j < escapedLabel.length() && escapedLabel.charAt(j) == '\\';
+      if (j == escapedLabel.length() || isEscapeCharacter) {
+        label.append(escapedLabel, i, j);
+        i = j + 2;
+      }
+      if (j < escapedLabel.length() && isEscapeCharacter) {
+        j++;
+        label.append(unescapeCharacter(escapedLabel.charAt(j)));
+      }
+    }
+    return label.toString();
+  }
+
+  private static char unescapeCharacter(char c) {
+    switch (c) {
+      case '\\':
+        return '\\';
+      case 'n':
+        return '\n';
+      case ':':
+        return ';';
+      default:
+        return c;
+    }
   }
 
   private static Observable<String> serialize(DirectedGraph<TaskId> graph) {
@@ -122,8 +244,7 @@ final class TaskFileSource {
         .flatMapObservable(Observable::fromIterable)
         .filter(node -> node.successors().isPopulated())
         .sorted(Comparator.comparing(node -> node.item().asLong()))
-        .map(TaskFileSource::serialize)
-        .concatMap(line -> Observable.just(line, END_OF_LINE));
+        .map(TaskFileSource::serialize);
   }
 
   private static String serialize(DirectedGraph.DirectedNode<TaskId> node) {
@@ -135,7 +256,8 @@ final class TaskFileSource {
                 .map(DirectedGraph.DirectedNode::item)
                 .sorted(Comparator.comparing(TaskId::asLong))
                 .map(TaskFileSource::serialize)
-                .collect(joining()))
+                .collect(joining(TASK_ID_DELIMITER)))
+        .append(TASK_FIELD_DELIMITER)
         .toString();
   }
 }
