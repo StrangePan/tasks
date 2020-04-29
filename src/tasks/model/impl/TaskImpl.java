fa24600b60fe40c5ef4e7006ef39ef9621714e5a
Dan Andrus <danieleandrus@gmail.com>
2020-04-29 01:04:50 -0700

    Add underlines to shortest prefix of task IDs

--- src/tasks/model/impl/TaskImpl.java
+++ src/tasks/model/impl/TaskImpl.java
@@ -4,9 +4,13 @@ import static java.util.Objects.requireNonNull;
 
 import io.reactivex.Completable;
 import io.reactivex.Flowable;
+import io.reactivex.Observable;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.function.Function;
 import omnia.data.structure.Set;
+import omnia.data.structure.mutable.MutableSet;
+import omnia.data.structure.mutable.TreeSet;
 import tasks.model.Task;
 import tasks.model.TaskMutator;
 
@@ -34,12 +38,44 @@ final class TaskImpl implements Task {
 
   @Override
   public String toString() {
+    String stringId = id.toString();
+    TreeSet<String> allIds =
+        store.allTasks()
+            .firstOrError()
+            .flatMapObservable(Observable::fromIterable)
+            .cast(TaskImpl.class)
+            .map(TaskImpl::id)
+            .map(Object::toString)
+            .collectInto(TreeSet.create(String::compareTo), MutableSet::add)
+            .blockingGet();
+    Optional<String> precedingId = allIds.itemPreceding(stringId);
+    Optional<String> followingId = allIds.itemFollowing(stringId);
+
+    int longestCommonPrefix = Math.max(
+        precedingId.map(other -> longestCommonPrefix(other, stringId)).orElse(0),
+        followingId.map(other -> longestCommonPrefix(other, stringId)).orElse(0)) + 1;
+    String formattingResetCode = "\033[0m";
+    String underlineInitCode = "\033[4m";
+    String formattedStringId =
+        underlineInitCode
+            + stringId.substring(0, longestCommonPrefix)
+            + formattingResetCode
+            + stringId.substring(longestCommonPrefix);
+
     return new StringBuilder()
-        .append(id.toString())
+        .append(formattedStringId)
         .append(isCompleted().blockingFirst() ? " (completed)" : "")
         .append(": ")
         .append(label().blockingFirst())
         .toString();
+  }
+
+  private static int longestCommonPrefix(String a, String b) {
+    for (int i = 0;; i++) {
+      if (i > a.length() || i > b.length() || a.charAt(i) != b.charAt(i)) {
+        return i;
+      }
+    }
   }
 
   @Override
