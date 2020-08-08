dd7f3e6d0e945171a048b987603bcfe6deb7f773
Dan Andrus <danieleandrus@gmail.com>
2020-08-08 12:21:22 -0700

    Create Options for list from Registration

--- src/tasks/cli/command/list/ListArguments.java
+++ src/tasks/cli/command/list/ListArguments.java
@@ -1,26 +1,64 @@
 package tasks.cli.command.list;
 
+import static omnia.data.cache.Memoized.memoize;
 import static tasks.cli.arg.CliArguments.Parameter.Repeatable.NOT_REPEATABLE;
 import static tasks.cli.arg.CliUtils.assertNoExtraArgs;
 import static tasks.cli.arg.CliUtils.tryParse;
 
+import omnia.data.cache.Memoized;
 import omnia.data.structure.immutable.ImmutableList;
 import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import tasks.cli.arg.CliArguments;
 import tasks.cli.arg.CliMode;
+import tasks.cli.arg.CliUtils;
 
 public final class ListArguments {
   private final boolean isUnblockedSet;
   private final boolean isBlockedSet;
   private final boolean isCompletedSet;
 
-  private ListArguments(boolean isUnblockedSet, boolean isBlockedSet, boolean isCompletedSet) {
-    this.isUnblockedSet = isUnblockedSet;
-    this.isBlockedSet = isBlockedSet;
-    this.isCompletedSet = isCompletedSet;
-  }
+  private static final Memoized<CliArguments.FlagOption> BLOCKED_OPTION =
+      memoize(
+          () -> new CliArguments.FlagOption(
+              "blocked",
+              "b",
+              "List all tasks that are uncompleted, but blocked by other tasks. Can "
+                  + "be combined with other flags.",
+              NOT_REPEATABLE));
+
+  private static final Memoized<CliArguments.FlagOption> COMPLETED_OPTION =
+      memoize(
+          () -> new CliArguments.FlagOption(
+              "completed",
+              "c",
+              "List all tasks already marked as completed. Can be combined with "
+                  + "other flags.",
+              NOT_REPEATABLE));
+
+  private static final Memoized<CliArguments.FlagOption> UNBLOCKED_OPTION =
+      memoize(
+          () -> new CliArguments.FlagOption(
+              "unblocked",
+              "u",
+              "List all unblocked tasks. Can be combined with other flags.",
+              NOT_REPEATABLE));
+
+  private static final Memoized<CliArguments.FlagOption> ALL_OPTION =
+      memoize(
+          () -> new CliArguments.FlagOption(
+              "all",
+              "a",
+              "Lists all tasks. A shortcut for all other flags put together.",
+              NOT_REPEATABLE));
+
+  private static final Memoized<ImmutableList<CliArguments.Option>> OPTIONS =
+      memoize(
+          () -> ImmutableList.of(
+              BLOCKED_OPTION.value(),
+              COMPLETED_OPTION.value(),
+              UNBLOCKED_OPTION.value(),
+              ALL_OPTION.value()));
 
   public static CliArguments.CommandRegistration registration() {
     return CliArguments.CommandRegistration.builder()
@@ -28,35 +66,18 @@ public final class ListArguments {
         .canonicalName("list")
         .aliases("ls", "l")
         .parameters(ImmutableList.empty())
-        .options(
-            ImmutableList.of(
-                new CliArguments.FlagOption(
-                    "blocked",
-                    "b",
-                    "List all tasks that are uncompleted, but blocked by other tasks. Can "
-                        + "be combined with other flags.",
-                    NOT_REPEATABLE),
-                new CliArguments.FlagOption(
-                    "completed",
-                    "c",
-                    "List all tasks already marked as completed. Can be combined with "
-                        + "other flags.",
-                    NOT_REPEATABLE),
-                new CliArguments.FlagOption(
-                    "unblocked",
-                    "u",
-                    "List all unblocked tasks. Can be combined with other flags.",
-                    NOT_REPEATABLE),
-                new CliArguments.FlagOption(
-                    "all",
-                    "a",
-                    "Lists all tasks. A shortcut for all other flags put together.",
-                    NOT_REPEATABLE)))
+        .options(OPTIONS.value())
         .parser(() -> ListArguments::parse)
         .helpDocumentation(
             "Prints a list of tasks. By default, only lists uncompleted tasks that are "
                 + "unblocked. Can also list only blocked tasks, only completed tasks, any "
                 + "combination of the three, or all tasks.");
+  }
+
+  private ListArguments(boolean isUnblockedSet, boolean isBlockedSet, boolean isCompletedSet) {
+    this.isUnblockedSet = isUnblockedSet;
+    this.isBlockedSet = isBlockedSet;
+    this.isCompletedSet = isCompletedSet;
   }
 
   public boolean isUnblockedSet() {
@@ -78,40 +99,19 @@ public final class ListArguments {
     optional --blocked flag
     optional --completed flag
     */
-    Options options = new Options();
-    options.addOption(
-        Option.builder("b")
-            .longOpt("blocked")
-            .desc("Setting this flag lists tasks that are uncompleted, but blocked by other tasks")
-            .numberOfArgs(0)
-            .build());
-    options.addOption(
-        Option.builder("c")
-            .longOpt("completed")
-            .desc("Setting this flag lists tasks that are already marked as completed.")
-            .numberOfArgs(0)
-            .build());
-    options.addOption(
-        Option.builder("u")
-            .longOpt("unblocked")
-            .desc("Setting this flag lists tasks that are unblocked. This is the default.")
-            .numberOfArgs(0)
-            .build());
-    options.addOption(
-        Option.builder("a")
-            .longOpt("all")
-            .desc("Setting this flag lists all tasks.")
-            .numberOfArgs(0)
-            .build());
+    Options options = CliUtils.toOptions(OPTIONS.value());
 
     CommandLine commandLine = tryParse(args, options);
     assertNoExtraArgs(commandLine);
 
-    boolean isAllSet = commandLine.hasOption("a");
-    boolean isBlockedSet = isAllSet || commandLine.hasOption("b");
-    boolean isCompletedSet = isAllSet || commandLine.hasOption("c");
+    boolean isAllSet = commandLine.hasOption(ALL_OPTION.value().shortName());
+    boolean isBlockedSet = isAllSet || commandLine.hasOption(BLOCKED_OPTION.value().shortName());
+    boolean isCompletedSet =
+        isAllSet || commandLine.hasOption(COMPLETED_OPTION.value().shortName());
     boolean isUnblockedSet =
-        isAllSet || commandLine.hasOption("u") || (!isBlockedSet && !isCompletedSet);
+        isAllSet
+            || commandLine.hasOption(UNBLOCKED_OPTION.value().shortName())
+            || (!isBlockedSet && !isCompletedSet);
 
     return new ListArguments(isUnblockedSet, isBlockedSet, isCompletedSet);
   }
