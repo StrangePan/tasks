b109570532dcfec1d25ac523328e57dfe0b7eacc
Daniel Andrus <danieleandrus@gmail.com>
2020-09-02 22:16:50 -0700

    Add StripColors option in a common place so it can be used in all commands.

---
+++ src/tasks/cli/command/StripColors.java
@@ -0,0 +1,20 @@
+package tasks.cli.command;
+
+import static omnia.data.cache.Memoized.memoize;
+import static tasks.cli.arg.CliArguments.Parameter.Repeatable.NOT_REPEATABLE;
+
+import omnia.data.cache.Memoized;
+import tasks.cli.arg.CliArguments;
+
+public final class StripColors {
+  private StripColors() {}
+
+  public static Memoized<CliArguments.FlagOption> STRIP_COLORS_OPTION =
+      memoize(
+          () -> new CliArguments.FlagOption(
+              "nocolor",
+              "c",
+              "When this flag is present, all color and formatting codes are stripped from the "
+                  + "output. This is useful when attempting to pipe the output.",
+              NOT_REPEATABLE));
+}
