00f61db25888075d5794c7d5406234dfa4529cf2
Dan Andrus <danieleandrus@gmail.com>
2020-09-30 08:35:55 -0700

    Assign columns to tasks for rendering

--- src/tasks/cli/command/graph/GraphHandler.java
+++ src/tasks/cli/command/graph/GraphHandler.java
@@ -1,13 +1,21 @@
 package tasks.cli.command.graph;
 
 import static java.util.Objects.requireNonNull;
+import static omnia.algorithm.GraphAlgorithms.topologicallySort;
 
 import io.reactivex.Observable;
 import io.reactivex.Single;
-import omnia.algorithm.GraphAlgorithms;
 import omnia.cli.out.Output;
 import omnia.data.cache.Memoized;
+import omnia.data.structure.DirectedGraph;
+import omnia.data.structure.DirectedGraph.DirectedNode;
 import omnia.data.structure.List;
+import omnia.data.structure.Map;
+import omnia.data.structure.Set;
+import omnia.data.structure.immutable.ImmutableMap;
+import omnia.data.structure.mutable.HashMap;
+import omnia.data.structure.mutable.MutableMap;
+import omnia.data.structure.tuple.Tuple;
 import tasks.cli.handler.ArgumentHandler;
 import tasks.model.Task;
 import tasks.model.TaskStore;
@@ -24,13 +32,62 @@ public final class GraphHandler implements ArgumentHandler<GraphArguments> {
   public Single<Output> handle(GraphArguments arguments) {
     return taskStore.value()
         .taskGraph()
-        .map(GraphAlgorithms::topologicallySort)
+        .map(graph -> Tuple.of(graph, topologicallySort(graph)))
+        .map(couple -> Tuple.of(couple.second(), assignColumns(couple.first(), couple.second())))
         .firstOrError()
-        .flatMap(this::renderGraph);
+        .flatMap(couple -> renderGraph(couple.first(), couple.second()));
   }
 
-  private Single<Output> renderGraph(List<Task> graph) {
-    return Observable.fromIterable(graph)
+  private Map<Task, Integer> assignColumns(DirectedGraph<Task> taskGraph, List<Task> taskList) {
+    MutableMap<Task, Integer> assignedColumns = HashMap.create();
+    int unresolvedEdges = 0;
+
+    for (Task taskToAssign : taskList) {
+      Set<? extends DirectedNode<Task>> predecessors =
+          taskGraph.nodeOf(taskToAssign).get().predecessors();
+
+      if (predecessors.isPopulated()) {
+
+        Task predecessorWithMinColumn =
+            predecessors.stream()
+                .map(DirectedNode::item)
+                .reduce(
+                    (p1, p2) ->
+                        assignedColumns.valueOf(p1).get() < assignedColumns.valueOf(p2).get()
+                            ? p1
+                            : p2)
+                .get();
+
+        int columnOfPredecessor = assignedColumns.valueOf(predecessorWithMinColumn).get();
+
+        int resolvedEdgesOfPredecessor =
+            taskGraph.nodeOf(predecessorWithMinColumn).get()
+                .successors()
+                .stream()
+                .map(DirectedNode::item)
+                .reduce(0,
+                    (sum, successor) ->
+                        sum + (assignedColumns.valueOf(successor).isPresent() ? 1 : 0),
+                    Integer::sum);
+
+        int assignedColumn = columnOfPredecessor + resolvedEdgesOfPredecessor;
+
+        unresolvedEdges--;
+        assignedColumns.putMapping(taskToAssign, assignedColumn);
+
+      } else {
+        int assignedColumn = unresolvedEdges;
+
+        unresolvedEdges--;
+        assignedColumns.putMapping(taskToAssign, assignedColumn);
+      }
+    }
+
+    return ImmutableMap.copyOf(assignedColumns);
+  }
+
+  private Single<Output> renderGraph(List<Task> tasks, Map<Task, Integer> columns) {
+    return Observable.fromIterable(tasks)
         .map(Task::render)
         .collectInto(Output.builder(), Output.Builder::appendLine)
         .map(Output.Builder::build);
