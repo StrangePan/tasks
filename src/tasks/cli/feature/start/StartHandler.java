07a75f6a9546adb6e7888a6f38e080f69f25d369
Dan Andrus <danieleandrus@gmail.com>
2020-11-13 23:59:11 -0800

    Add Start command

---
+++ src/tasks/cli/feature/start/StartHandler.java
@@ -0,0 +1,91 @@
+package tasks.cli.feature.start;
+
+import static java.util.Objects.requireNonNull;
+import static omnia.data.stream.Collectors.toImmutableSet;
+import static tasks.cli.handler.HandlerUtil.stringifyIfPopulated;
+
+import io.reactivex.Observable;
+import io.reactivex.Single;
+import java.util.Optional;
+import omnia.cli.out.Output;
+import omnia.data.cache.Memoized;
+import omnia.data.structure.immutable.ImmutableSet;
+import omnia.data.structure.tuple.Tuplet;
+import tasks.cli.handler.ArgumentHandler;
+import tasks.cli.handler.HandlerException;
+import tasks.model.ObservableTaskStore;
+import tasks.model.Task;
+import tasks.model.TaskId;
+
+/** Business logic for the Start command. */
+public final class StartHandler implements ArgumentHandler<StartArguments> {
+  private final Memoized<? extends ObservableTaskStore> taskStore;
+
+  public StartHandler(Memoized<? extends ObservableTaskStore> taskStore) {
+    this.taskStore = requireNonNull(taskStore);
+  }
+
+  @Override
+  public Single<Output> handle(StartArguments arguments) {
+    // Validate arguments
+    if (!arguments.tasks().isPopulated()) {
+      throw new HandlerException("no tasks specified");
+    }
+
+    ObservableTaskStore taskStore = this.taskStore.value();
+
+    return Observable.fromIterable(arguments.tasks())
+        .flatMapSingle(task -> taskStore.mutateTask(task, mutator -> mutator.setStatus(Task.Status.STARTED)))
+        .reduce(
+            Tuplet.of(
+                ImmutableSet.<TaskId>builder(), // tasks that were already started
+                ImmutableSet.<TaskId>builder(), // tasks that were marked as started
+                ImmutableSet.<TaskId>builder()), // tasks that became blocked
+            (builders, mutationResult) -> {
+              boolean becameStarted =
+                  mutationResult.first()
+                      .lookUpById(mutationResult.third().id())
+                      .map(task -> !task.status().isStarted())
+                      .orElse(false);
+              (becameStarted ? builders.second() : builders.first())
+                  .add(mutationResult.third().id());
+
+              if (becameStarted) {
+                mutationResult.third()
+                    .blockedTasks()
+                    .stream()
+                    .map(Task::id)
+                    .forEach(builders.third()::add);
+              }
+
+              return builders;
+            })
+        .map(groupedTasks -> groupedTasks.map(ImmutableSet.Builder::build))
+        .flatMap(
+            groupedTasks -> taskStore.observe()
+                .firstOrError()
+                .map(
+                    store -> groupedTasks.map(
+                        list -> list.stream()
+                            .map(store::lookUpById)
+                            .map(Optional::orElseThrow)
+                            .collect(toImmutableSet()))))
+        .map(
+            groupedTasks -> Tuplet.of(
+                groupedTasks.first(),
+                groupedTasks.second(),
+                groupedTasks.third()
+                    .stream()
+                    .filter(task -> !task.status().isCompleted())
+                    .collect(toImmutableSet())))
+        .flatMapObservable(Observable::fromIterable)
+        .zipWith(
+            Observable.just(
+                "task(s) already started:",
+                "task(s) started:",
+                "task(s) blocked as a result:"),
+            (groupedTasks, header) -> stringifyIfPopulated(header, groupedTasks))
+        .collectInto(Output.builder(), Output.Builder::append)
+        .map(Output.Builder::build);
+  }
+}
