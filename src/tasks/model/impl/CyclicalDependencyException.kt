e1ce459a568f6c4f9e9b01bfd2bd87ab7c788897
Dan Andrus <danieleandrus@gmail.com>
2020-12-31 23:30:58 -0800

    Revert "Merge branch 'more'"
    This reverts commit fb5bce278e8ef5f42ae073ea66cdf10b2a83e1ff, reversing
    changes made to 06028577ba03467c8fcc63155a7866b9ff522bad.

--- src/tasks/model/impl/CyclicalDependencyException.kt
+++ src/tasks/model/impl/CyclicalDependencyException.kt
@@ -1,6 +1,5 @@
 package tasks.model.impl
 
-import java.lang.System.lineSeparator
 import omnia.data.structure.List
 import omnia.data.structure.immutable.ImmutableList
 
@@ -11,10 +10,10 @@ class CyclicalDependencyException internal constructor(msg: String, cycle: List<
     private const val serialVersionUID = -1193897049921304555L
 
     private fun buildMessage(prefix: String, cycle: List<*>): String {
-      val message = StringBuilder().append(prefix).append(lineSeparator())
-      message.append("->").append(cycle.itemAt(0)).append(lineSeparator())
+      val message = StringBuilder().append(prefix).append("\n")
+      message.append("->").append(cycle.itemAt(0)).append("\n")
       for (i in 1 until cycle.count()) {
-        message.append("| ").append(cycle.itemAt(i)).append(lineSeparator())
+        message.append("| ").append(cycle.itemAt(i)).append("\n")
       }
       message.append("--").append(cycle.itemAt(0))
       return message.toString()
